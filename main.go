7c580a8e5abc51fb81f93c31c86701a2a976ffad
Andrew Snodgrass <asnodgrass@anypresence.com>
2016-01-08 20:41:11 -0700

    Use multiple vectorizers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"image/color"
 	"log"
@@ -12,10 +13,12 @@ import (
 	"time"
 
 	"github.com/goml/gobrain"
+	"github.com/gonum/matrix/mat64"
 	"github.com/gonum/plot"
 	//"github.com/gonum/plot/plotter"
 	"github.com/gonum/plot/vg"
 	"github.com/gonum/plot/vg/draw"
+	"github.com/kortschak/nmf"
 	"github.com/pointlander/kmeans"
 	"github.com/skelterjohn/go.matrix"
 )
@@ -24,12 +27,20 @@ const (
 	NEURAL_WIDTH  = 4
 	NEURAL_MIDDLE = 3
 	MODEL_WIDTH   = (NEURAL_WIDTH+1)*(NEURAL_MIDDLE+1) + (NEURAL_MIDDLE+1)*NEURAL_WIDTH
+	NMF_WIDTH     = 2
+	SAMPLES       = 4
+	LAST_SAMPLE   = SAMPLES - 1
 )
 
-type Vector [MODEL_WIDTH]float64
+type Vectorizer struct {
+	Abbr, Name string
+	Vectorize  func(prices []Quote, minYear, maxYear int) *matrix.DenseMatrix
+}
+
+type Vector []float64
 
 /* https://en.wikipedia.org/wiki/Cosine_similarity */
-func (a *Vector) similarity(b *Vector) float64 {
+func (a Vector) similarity(b Vector) float64 {
 	cols := len(a)
 	dot, maga, magb := float64(0), float64(0), float64(0)
 	for j := 0; j < cols; j++ {
@@ -238,7 +249,309 @@ func (cp *CPoints) DataRange() (xmin, xmax, ymin, ymax float64) {
 	return
 }
 
+func yearIterator(prices []Quote, iterate func(year int, patterns [][][]float64)) {
+	patterns, i, size, min, max := [][][]float64{}, 0, len(prices), math.MaxFloat64, float64(0)
+	normalize := func() {
+		diff := max - min
+		for _, pattern := range patterns {
+			for _, a := range pattern {
+				for b, _ := range a {
+					a[b] = (a[b] - min) / diff
+				}
+			}
+		}
+	}
+	for i+3*7 < size {
+		if prices[i].t.Year() != prices[i+3*7].t.Year() {
+			normalize()
+			iterate(prices[i].t.Year(), patterns)
+			patterns, min, max = [][][]float64{}, math.MaxFloat64, float64(0)
+			i += 3 * 7
+			if i+3*7 >= size {
+				break
+			}
+		}
+		in, out := make([]float64, 4), make([]float64, 4)
+		for j := 0; j < 4; j++ {
+			price := prices[i+j*7].price
+			if price < min {
+				min = price
+			}
+			if price > max {
+				max = price
+			}
+			in[j] = price
+			out[j] = price
+		}
+		patterns = append(patterns, [][]float64{in, out})
+		i++
+	}
+	if len(patterns) > 0 {
+		normalize()
+		iterate(prices[i-1].t.Year(), patterns)
+	}
+}
+
+func rnnVectorizer(prices []Quote, minYear, maxYear int) *matrix.DenseMatrix {
+	iterate := func(year int, patterns [][][]float64) {
+		maxYear = year
+	}
+	yearIterator(prices, iterate)
+
+	model := matrix.Zeros(maxYear-minYear+1, MODEL_WIDTH)
+	iterate = func(year int, patterns [][][]float64) {
+		fmt.Println(year)
+		rand.Seed(1)
+		ff := &gobrain.FeedForward{}
+		ff.Init(NEURAL_WIDTH, NEURAL_MIDDLE, NEURAL_WIDTH)
+		ff.SetContexts(7, nil)
+		ff.Train(patterns, 1, 0.6, 0.4, true)
+		i, j := year-minYear, 0
+		for _, weights := range ff.InputWeights {
+			for _, weight := range weights {
+				model.Set(i, j, weight)
+				j++
+			}
+		}
+		for _, weights := range ff.OutputWeights {
+			for _, weight := range weights {
+				model.Set(i, j, weight)
+				j++
+			}
+		}
+	}
+	yearIterator(prices, iterate)
+
+	return model
+}
+
+func rnniVectorizer(prices []Quote, minYear, maxYear int) *matrix.DenseMatrix {
+	iterate := func(year int, patterns [][][]float64) {
+		maxYear = year
+	}
+	yearIterator(prices, iterate)
+
+	model := matrix.Zeros(maxYear-minYear+1, maxYear-minYear+1)
+	iterate = func(year int, patterns [][][]float64) {
+		fmt.Println(year)
+		rand.Seed(1)
+		ff := &gobrain.FeedForward{}
+		ff.Init(NEURAL_WIDTH, NEURAL_MIDDLE, NEURAL_WIDTH)
+		ff.SetContexts(7, nil)
+		ff.Train(patterns, 1, 0.6, 0.4, true)
+		iterate := func(y int, patterns [][][]float64) {
+			err := 0.0
+			ff.SetContexts(7, nil)
+			for _, pattern := range patterns {
+				out := ff.Update(pattern[0])
+				for a, b := range out {
+					c := b - pattern[0][a]
+					err += c * c
+				}
+			}
+			model.Set(year-minYear, y-minYear, err/float64(len(patterns)))
+		}
+		yearIterator(prices, iterate)
+	}
+	yearIterator(prices, iterate)
+
+	return model
+}
+
+func posNorm(_, _ int, _ float64) float64 {
+	return math.Abs(rand.NormFloat64())
+}
+
+func nmfVectorizer(prices []Quote, minYear, maxYear int) *matrix.DenseMatrix {
+	model, size := matrix.Zeros(maxYear-minYear+1, SAMPLES*NMF_WIDTH), len(prices)
+	conf := nmf.Config{
+		Tolerance:   1e-3,
+		MaxIter:     100,
+		MaxOuterSub: 1000,
+		MaxInnerSub: 20,
+		Limit:       30 * time.Minute,
+	}
+	factor := func(year int, data []float64) {
+		cols, rows := SAMPLES, len(data)/SAMPLES
+		rand.Seed(1)
+		V := mat64.NewDense(rows, cols, data)
+		Wo := mat64.NewDense(rows, NMF_WIDTH, nil)
+		Wo.Apply(posNorm, Wo)
+		Ho := mat64.NewDense(NMF_WIDTH, cols, nil)
+		Ho.Apply(posNorm, Ho)
+		W, H, ok := nmf.Factors(V, Wo, Ho, conf)
+		if !ok {
+			fmt.Println("matrix factoring failed")
+		}
+		_ = W
+		k := 0
+		for i := 0; i < cols; i++ {
+			for j := 0; j < NMF_WIDTH; j++ {
+				model.Set(year-minYear, k, H.At(j, i))
+				k++
+			}
+		}
+	}
+	data, i := []float64{}, 0
+	for i+LAST_SAMPLE*7 < size {
+		if prices[i].t.Year() != prices[i+LAST_SAMPLE*7].t.Year() {
+			fmt.Println(prices[i].t)
+			factor(prices[i].t.Year(), data)
+			data = []float64{}
+			i += LAST_SAMPLE * 7
+			if i+LAST_SAMPLE*7 >= size {
+				break
+			}
+		}
+		for j := 0; j < SAMPLES; j++ {
+			data = append(data, prices[i+j*7].price)
+		}
+		i++
+	}
+	fmt.Println(prices[i-1].t)
+	if len(data) > 0 {
+		factor(prices[i-1].t.Year(), data)
+	}
+	return model
+}
+
+var vectorizers = []Vectorizer{
+	{"nmf", "non-negative matrix factorization", nmfVectorizer},
+	{"rnn", "recurrent neural network", rnnVectorizer},
+	{"rnni", "recurrent neural network indirect", rnniVectorizer},
+}
+
+func (v *Vectorizer) Graph(prices []Quote, minYear, maxYear int) {
+	model := v.Vectorize(prices, minYear, maxYear)
+	//norm(model)
+	rows, cols := model.Rows(), model.Cols()
+	rawData := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		rawData[i] = make([]float64, cols)
+		for j := 0; j < cols; j++ {
+			rawData[i][j] = model.Get(i, j)
+		}
+	}
+	labels, _, err := kmeans.Kmeans(rawData, 3, kmeans.CanberraDistance, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := PCA(model.Copy(), 2)
+	rows = r.Rows()
+	points := make([]CPoint, rows)
+	for i := 0; i < rows; i++ {
+		points[i].X, points[i].Y, points[i].C =
+			r.Get(i, 0), r.Get(i, 1), labels[i]
+	}
+	marketDynamics := &CPoints{points, false}
+
+	p, err := plot.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.Title.Text = v.Name + " Market Dynamics"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	p.Add(marketDynamics)
+	if err := p.Save(512, 512, v.Abbr+"_market_dynamics.png"); err != nil {
+		log.Fatal(err)
+	}
+
+	ticks := []plot.Tick{}
+	for _, price := range prices {
+		if price.t.Year()%2 == 0 && price.t.YearDay() == 1 {
+			ticks = append(ticks, plot.Tick{float64(price.t.Unix()), fmt.Sprintf("%v", price.t.Year())})
+		}
+	}
+
+	cpoints := make([]CPoint, len(prices))
+	for i, price := range prices {
+		cpoints[i].X, cpoints[i].Y, cpoints[i].C =
+			float64(price.t.Unix()), price.price, -1 //labels[price.t.Year()-minYear]
+	}
+	marketPrices := &CPoints{cpoints, false}
+	xmin, xmax, ymin, ymax := marketDynamics.DataRange()
+	for i, price := range prices {
+		index := price.t.Year() - minYear
+		if index < len(points) {
+			point := points[index]
+			cpoints[i].R = byte(255 * (point.X - xmin) / (xmax - xmin))
+			cpoints[i].B = byte(255 * (point.Y - ymin) / (ymax - ymin))
+		} else {
+			cpoints[i].R = 255
+		}
+	}
+
+	p, err = plot.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.Title.Text = v.Name + " DJIA"
+	p.X.Label.Text = "Time"
+	p.Y.Label.Text = "Price"
+	p.X.Tick.Marker = plot.ConstantTicks(ticks)
+	p.Add(marketPrices)
+	if err := p.Save(2048, 2048, v.Abbr+"_market_prices.png"); err != nil {
+		log.Fatal(err)
+	}
+
+	var markov [3][3]int
+	last := 0
+	for _, label := range labels {
+		markov[last][label]++
+		last = label
+	}
+	colors := [...]string{"Green", "Red", "Blue"}
+	for j, a := range markov {
+		sum := 0.0
+		for _, b := range a {
+			sum += float64(b)
+		}
+		fmt.Println(colors[j])
+		for i, b := range a {
+			fmt.Printf("%.1f %v\n", 100*float64(b)/sum, colors[i])
+		}
+		fmt.Printf("\n")
+	}
+
+	rows, cols = model.Rows(), model.Cols()
+	angles := make([]float64, rows)
+	for i := 1; i < rows; i++ {
+		a, b := make(Vector, cols), make(Vector, cols)
+		for j := 0; j < cols; j++ {
+			a[j], b[j] = model.Get(i, j), model.Get(i-1, j)
+		}
+		angles[i] = a.similarity(b)
+	}
+
+	points = make([]CPoint, rows)
+	for i := 0; i < rows; i++ {
+		unix := time.Date(minYear+i, time.January, 0, 0, 0, 0, 0, time.UTC).Unix()
+		points[i].X, points[i].Y, points[i].C =
+			float64(unix), angles[i], labels[i]
+	}
+	marketSimilarity := &CPoints{points, true}
+
+	p, err = plot.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.Title.Text = v.Name + " Market Similarity"
+	p.X.Label.Text = "Time"
+	p.Y.Label.Text = "Similarity"
+	p.X.Tick.Marker = plot.ConstantTicks(ticks)
+	p.Add(marketSimilarity)
+	if err := p.Save(2048, 2048, v.Abbr+"_market_similarity.png"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var vectorizer = flag.String("vectorizer", "all", "all/nmf/rnn/rnni")
+
 func main() {
+	flag.Parse()
+
 	in, err := os.Open("UDJIAD1.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -267,185 +580,17 @@ func main() {
 		prices[i].t = t
 		prices[i].price = price
 	})
-	model := matrix.Zeros(maxYear-minYear+1, MODEL_WIDTH)
-	train := func(year int, patterns [][][]float64) {
-		min, max := math.MaxFloat64, float64(0)
-		for _, pattern := range patterns {
-			for _, a := range pattern {
-				for _, b := range a {
-					if b < min {
-						min = b
-					}
-					if b > max {
-						max = b
-					}
-				}
-			}
+
+	if *vectorizer == "all" {
+		for _, v := range vectorizers {
+			v.Graph(prices, minYear, maxYear)
 		}
-		d := max - min
-		for _, pattern := range patterns {
-			for _, a := range pattern {
-				for b, _ := range a {
-					a[b] = (a[b] - min) / d
-				}
-			}
-		}
-		rand.Seed(0)
-		ff := &gobrain.FeedForward{}
-		ff.Init(NEURAL_WIDTH, NEURAL_MIDDLE, NEURAL_WIDTH)
-		ff.SetContexts(7, nil)
-		ff.Train(patterns, 1, 0.6, 0.4, true)
-		i, j := year-minYear, 0
-		for _, weights := range ff.InputWeights {
-			for _, weight := range weights {
-				model.Set(i, j, weight)
-				j++
-			}
-		}
-		for _, weights := range ff.OutputWeights {
-			for _, weight := range weights {
-				model.Set(i, j, weight)
-				j++
-			}
-		}
-	}
-	patterns, i := [][][]float64{}, 0
-	for i+3*7 < size {
-		if prices[i].t.Year() != prices[i+3*7].t.Year() {
-			fmt.Println(prices[i].t)
-			train(prices[i].t.Year(), patterns)
-			patterns = [][][]float64{}
-			i += 3 * 7
-			if i+3*7 >= size {
+	} else {
+		for _, v := range vectorizers {
+			if v.Abbr == *vectorizer {
+				v.Graph(prices, minYear, maxYear)
 				break
 			}
 		}
-		in, out := make([]float64, 4), make([]float64, 4)
-		for j := 0; j < 4; j++ {
-			in[j] = prices[i+j*7].price
-			out[j] = prices[i+j*7].price
-		}
-		patterns = append(patterns, [][]float64{in, out})
-		i++
-	}
-	fmt.Println(prices[i-1].t)
-	train(prices[i-1].t.Year(), patterns)
-
-	//norm(model)
-	rows, cols := model.Rows(), model.Cols()
-	rawData := make([][]float64, rows)
-	for i := 0; i < rows; i++ {
-		rawData[i] = make([]float64, cols)
-		for j := 0; j < cols; j++ {
-			rawData[i][j] = model.Get(i, j)
-		}
-	}
-	labels, _, err := kmeans.Kmeans(rawData, 3, kmeans.CanberraDistance, 10)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r := PCA(model.Copy(), 2)
-	rows = r.Rows()
-	points := make([]CPoint, rows)
-	for i := 0; i < rows; i++ {
-		points[i].X, points[i].Y, points[i].C =
-			r.Get(i, 0), r.Get(i, 1), labels[i]
-	}
-	marketDynamics := &CPoints{points, false}
-
-	p, err := plot.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	p.Title.Text = "Market Dynamics"
-	p.X.Label.Text = "X"
-	p.Y.Label.Text = "Y"
-	p.Add(marketDynamics)
-	if err := p.Save(512, 512, "market_dynamics.png"); err != nil {
-		log.Fatal(err)
-	}
-
-	ticks := []plot.Tick{}
-	for _, price := range prices {
-		if price.t.Year()%2 == 0 && price.t.YearDay() == 1 {
-			ticks = append(ticks, plot.Tick{float64(price.t.Unix()), fmt.Sprintf("%v", price.t.Year())})
-		}
-	}
-
-	cpoints := make([]CPoint, len(prices))
-	for i, price := range prices {
-		cpoints[i].X, cpoints[i].Y, cpoints[i].C =
-			float64(price.t.Unix()), price.price, -1 //labels[price.t.Year()-minYear]
-	}
-	marketPrices := &CPoints{cpoints, false}
-	xmin, xmax, ymin, ymax := marketDynamics.DataRange()
-	for i, price := range prices {
-		point := points[price.t.Year()-minYear]
-		cpoints[i].R = byte(255 * (point.X - xmin) / (xmax - xmin))
-		cpoints[i].B = byte(255 * (point.Y - ymin) / (ymax - ymin))
-	}
-
-	p, err = plot.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	p.Title.Text = "DJIA"
-	p.X.Label.Text = "Time"
-	p.Y.Label.Text = "Price"
-	p.X.Tick.Marker = plot.ConstantTicks(ticks)
-	p.Add(marketPrices)
-	if err := p.Save(2048, 2048, "market_prices.png"); err != nil {
-		log.Fatal(err)
-	}
-
-	var markov [3][3]int
-	last := 0
-	for _, label := range labels {
-		markov[last][label]++
-		last = label
-	}
-	colors := [...]string{"Green", "Red", "Blue"}
-	for j, a := range markov {
-		sum := 0.0
-		for _, b := range a {
-			sum += float64(b)
-		}
-		fmt.Println(colors[j])
-		for i, b := range a {
-			fmt.Printf("%.1f %v\n", 100*float64(b)/sum, colors[i])
-		}
-		fmt.Printf("\n")
-	}
-
-	rows, cols = model.Rows(), model.Cols()
-	angles := make([]float64, rows)
-	for i := 1; i < rows; i++ {
-		a, b := &Vector{}, &Vector{}
-		for j := 0; j < cols; j++ {
-			a[j], b[j] = model.Get(i, j), model.Get(i-1, j)
-		}
-		angles[i] = a.similarity(b)
-	}
-
-	points = make([]CPoint, rows)
-	for i := 0; i < rows; i++ {
-		unix := time.Date(minYear+i, time.January, 0, 0, 0, 0, 0, time.UTC).Unix()
-		points[i].X, points[i].Y, points[i].C =
-			float64(unix), angles[i], labels[i]
-	}
-	marketSimilarity := &CPoints{points, true}
-
-	p, err = plot.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	p.Title.Text = "Market Similarity"
-	p.X.Label.Text = "Time"
-	p.Y.Label.Text = "Similarity"
-	p.X.Tick.Marker = plot.ConstantTicks(ticks)
-	p.Add(marketSimilarity)
-	if err := p.Save(2048, 2048, "market_similarity.png"); err != nil {
-		log.Fatal(err)
 	}
 }
